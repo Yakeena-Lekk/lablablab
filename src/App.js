91566f19d9d5b50d9ca6562add25f6bd145ba3dc
Yakeena <s593512074@phuket.psu.ac.th>
2020-09-17 20:47:30 +0700

    componenets

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './App.css';
 import CharacterCard from './CharacterCard';
 
-  function App() {
-    return (
-    <div>
-      <CharacterCard value="h"/>
-      <CharacterCard value="i"/>
-    </div>
-    );
-  }
+const word = "Hello";
+function App() {
+ return (
+ <div>
+ {
+ Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+ }
+ </div>
+ );
+}
 
   export default App;
 
